592cd02152252e259f434282386c05bec6548047
Shubidumdu <skaty4870@naver.com>
2021-08-08 00:38:48 +0900

    Add resize event on canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import './index.scss';
+
+const canvas: HTMLCanvasElement = document.getElementById(
+  'canvas',
+) as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+canvas.style.background = 'black';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
