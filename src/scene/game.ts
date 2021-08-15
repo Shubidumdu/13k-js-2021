c91c7df4fdb443efe2cf2cdf593bf62759264f31
Shubidumdu <skaty4870@naver.com>
2021-08-15 15:35:13 +0900

    Add breathing animation on roach

--- src/scene/game.ts
+++ src/scene/game.ts
@@ -3,10 +3,12 @@ import { degreeToRadian } from '../utils';
 
 const TILE_WIDTH = 120;
 const TILE_HEIGHT = 40;
+let time = 0;
 
 export const drawGame = () => {
+  time += 1;
   drawMap(4, canvas.width / 2 + 40, canvas.height / 2, TILE_WIDTH, TILE_HEIGHT);
-  drawPlayer(2, 0);
+  drawPlayer(0, 0);
 };
 
 const drawPlayer = (x: number, y: number) => {
@@ -15,7 +17,7 @@ const drawPlayer = (x: number, y: number) => {
     context.setTransform(1, 0, 0, 2, canvas.width / 2, canvas.height / 2);
     context.arc(
       -140 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -10 + (y * TILE_HEIGHT) / 2,
+      -10 + (y * TILE_HEIGHT) / 2 + Math.sin(time / 20),
       20,
       0,
       degreeToRadian(360),
@@ -30,14 +32,14 @@ const drawPlayer = (x: number, y: number) => {
     context.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
     context.arc(
       -140 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -40 + y * TILE_HEIGHT,
+      -40 + y * TILE_HEIGHT + Math.sin(time / 20),
       3,
       0,
       degreeToRadian(360),
     );
     context.arc(
       -130 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -40 + y * TILE_HEIGHT,
+      -40 + y * TILE_HEIGHT + Math.sin(time / 20),
       3,
       0,
       degreeToRadian(360),
@@ -47,28 +49,45 @@ const drawPlayer = (x: number, y: number) => {
     context.fillStyle = '#000';
     context.fill();
   });
+  // draw(() => {
+  //   context.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
+  //   context.fillStyle = '#f32';
+  //   context.fillRect(
+  //     -120 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
+  //     -80 + y * TILE_HEIGHT,
+  //     10,
+  //     60,
+  //   );
+  //   context.fillStyle = '#aaa';
+  //   context.fillRect(
+  //     -120 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
+  //     -20 + y * TILE_HEIGHT,
+  //     10,
+  //     20,
+  //   );
+  // });
   draw(() => {
     // arms
     context.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
     context.moveTo(
       -150 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -30 + y * TILE_HEIGHT,
+      -30 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.quadraticCurveTo(
       -150 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      0 + y * TILE_HEIGHT,
+      0 + y * TILE_HEIGHT + Math.sin(time / 20),
       -110 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -10 + y * TILE_HEIGHT,
+      -10 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.moveTo(
       -122 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -30 + y * TILE_HEIGHT,
+      -30 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.quadraticCurveTo(
       -120 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      0 + y * TILE_HEIGHT,
+      0 + y * TILE_HEIGHT + Math.sin(time / 20),
       -110 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -10 + y * TILE_HEIGHT,
+      -10 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.strokeStyle = '#000';
     context.stroke();
@@ -78,21 +97,21 @@ const drawPlayer = (x: number, y: number) => {
     context.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
     context.moveTo(
       -150 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -0 + y * TILE_HEIGHT,
+      -0 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.quadraticCurveTo(
-      -145 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      0 + y * TILE_HEIGHT,
+      -145 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6 + Math.sin(time / 20),
+      0 + y * TILE_HEIGHT + Math.sin(time / 20),
       -150 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
       25 + y * TILE_HEIGHT,
     );
     context.moveTo(
       -124 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -0 + y * TILE_HEIGHT,
+      -0 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.quadraticCurveTo(
-      -120 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      0 + y * TILE_HEIGHT,
+      -120 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6 + Math.sin(time / 20),
+      0 + y * TILE_HEIGHT + Math.sin(time / 20),
       -120 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
       20 + y * TILE_HEIGHT,
     );
@@ -103,23 +122,23 @@ const drawPlayer = (x: number, y: number) => {
     context.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
     context.moveTo(
       -150 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -50 + y * TILE_HEIGHT,
+      -50 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.quadraticCurveTo(
-      -130 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -100 + y * TILE_HEIGHT,
-      -160 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -80 + y * TILE_HEIGHT,
+      -130 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6 - Math.sin(time / 20),
+      -100 + y * TILE_HEIGHT + Math.sin(time / 20),
+      -160 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6 - Math.sin(time / 20),
+      -80 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.moveTo(
       -130 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -50 + y * TILE_HEIGHT,
+      -50 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.quadraticCurveTo(
-      -100 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -100 + y * TILE_HEIGHT,
-      -140 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6,
-      -80 + y * TILE_HEIGHT,
+      -100 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6 - Math.sin(time / 20),
+      -100 + y * TILE_HEIGHT + Math.sin(time / 20),
+      -140 + x * TILE_WIDTH - (y * TILE_WIDTH) / 6 - Math.sin(time / 20),
+      -80 + y * TILE_HEIGHT + Math.sin(time / 20),
     );
     context.stroke();
   });
